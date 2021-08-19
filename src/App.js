c7022668775ec26c9bd5c99e162917c2da3930ed
Vignesh Elangovan <vigneshe1992@gmail.com>
2021-08-19 12:34:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world and gold
+          Hello world and gold and lord
         </p>
         <a
           className="App-link"
